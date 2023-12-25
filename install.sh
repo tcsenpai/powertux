4809ce680a3a881e433a4cc9b57a72c1d1628602
thecookingsenpai <elmastrococinero@gmail.com>
2023-12-25 13:29:26 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+THIS=$(pwd)
+SHELLTYPE=$(echo "${SHELL}" | awk -F"/" '{print $4}')
+echo "${SHELLTYPE}"
+SHELLRC='.'${SHELLTYPE}'rc'
+echo "${SHELLRC}"
+
+if [[ ! -f ${HOME}/"${SHELLRC}" ]]; then
+	echo "No suitable configuration file found for ${SHELLTYPE} (tested ${SHELLRC})"
+	exit 0
+fi
+
+echo "Configuration is ${HOME}/${SHELLRC}"
+
+read -p "Do you want to install the binaries system wide (in /usr/bin)? This requires root (yes/no)" yn
+case ${yn} in
+yes) sudo cp powertux_folder/bin/* /usr/bin/ ;;
+no) echo "Ok" ;;
+esac
+
+cp -R powertux_folder "${HOME}"/
+echo "PATH=${PATH}:${HOME}/powertux_folder/bin" >>"${HOME}"/"${SHELLRC}"
+echo "sh ${HOME}/powertux_folder/powertux.sh" >>"${HOME}"/"${SHELLRC}"
+cd "${THIS}" || exit
+echo "Done! Add your binaries into ${HOME}/powertux_folder/bin"
+echo "Please restart your shell or source your ${SHELLRC} to apply the settings in this session"
---
+++ powertux_folder/powertux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "TCSenpai's Custom Script"
+echo "Active customizations:"
+echo "> gitup; add, commit, lint, check, autocorrect, recommit and push all in one"
+echo "> pathis: add the current path to the PATH variable"
